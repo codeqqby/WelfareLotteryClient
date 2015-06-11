05f786e9b28cfd8ad1dedb699946ea7bdfd51d18
test <test>
2015-06-11 15:35:24 +0800

    图片浏览的UserControl

---
+++ WelfareLotteryClient/UserControls/UploadImage/ImageViewer.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WelfareLotteryClient.DBModels;
+
+namespace WelfareLotteryClient.UserControls.UploadImage
+{
+    /// <summary>
+    /// ImageViewer.xaml 的交互逻辑
+    /// </summary>
+    public partial class ImageViewer : UserControl
+    {
+        public PhotoCollection Photos;
+        public ImageViewer()
+        {
+            InitializeComponent();
+            Photos = (PhotoCollection)(Resources["Photos"] as ObjectDataProvider).Data;
+            gridContainer.DataContext = Photos;
+        }
+
+        private void OnPhotoClick(object sender, RoutedEventArgs e)
+        {
+            PhotoView pvWindow = new PhotoView
+            {
+                SelectedPhoto = (Photo) PhotosListBox.SelectedItem,
+                Owner =Application.Current.MainWindow
+            };
+            pvWindow.ShowDialog();
+        }
+
+        private void GroupBox_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+            //去掉文件夹 和 不是以图片后缀结果的文件
+
+            var aa= ((string[]) e.Data.GetData(DataFormats.FileDrop)).Where(b => !Directory.Exists(b) && Extension(b)).ToList();
+
+            if (!aa.Any())
+            {
+                MessageBox.Show("只能托入" + string.Join("", _extension) + "结尾的图片");
+                return;
+            }
+
+            if (MaxDropPhoto.HasValue && aa.Count > MaxDropPhoto.Value)
+            {
+                MessageBox.Show($"最多只能添加{MaxDropPhoto.Value}张图片");
+                return;
+            }
+
+            Photos.Update(aa);
+
+        }
+
+        readonly string[] _extension={".jpg",".png", ".gif", ".bmp", ".jpeg"};
+
+        public bool Extension(string path)
+        {
+            return _extension.Any(p => p == Path.GetExtension(path)?.ToLower());
+        }
+
+        //propdp
+        /// <summary>
+        /// 可托拽最大文件数
+        /// </summary>
+        public int? MaxDropPhoto
+        {
+            get { return (int?)GetValue(MaxDropPhotoProperty); }
+            set { SetValue(MaxDropPhotoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MaxDropPhoto.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxDropPhotoProperty =
+            DependencyProperty.Register("MaxDropPhoto", typeof(int?), typeof(ImageViewer), new PropertyMetadata(null));
+
+
+
+    }
+       
+}
---
+++ WelfareLotteryClient/UserControls/UploadImage/PhotoView.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WelfareLotteryClient.DBModels;
+
+namespace WelfareLotteryClient.UserControls.UploadImage
+{
+    /// <summary>
+    /// PhotoView.xaml 的交互逻辑
+    /// </summary>
+    public partial class PhotoView : Window
+    {
+        Photo _photo;
+
+        public PhotoView()
+        {
+            InitializeComponent();
+        }
+
+        public Photo SelectedPhoto
+        {
+            get { return _photo; }
+            set { _photo = value; }
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            ViewedPhoto.Source = _photo.Image;
+           // ViewedCaption.Content = _photo.Source;
+        }
+
+        private void Rotate(object sender, RoutedEventArgs e)
+        {
+            RotatePhoto();
+        }
+
+        private void RotatePhoto()
+        {
+            BitmapSource img = (BitmapSource) (_photo.Image);
+
+            CachedBitmap cache = new CachedBitmap(img, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            _photo.Image = BitmapFrame.Create(new TransformedBitmap(cache, new RotateTransform(90.0)));
+
+            ViewedPhoto.Source = _photo.Image;
+        }
+
+        private void Crop(object sender, RoutedEventArgs e)
+        {
+            BitmapSource img = (BitmapSource)(_photo.Image);
+
+            int halfWidth = img.PixelWidth / 2;
+            int halfHeight = img.PixelHeight / 2;
+            _photo.Image = BitmapFrame.Create(new CroppedBitmap(img, new Int32Rect((halfWidth - (halfWidth / 2)), (halfHeight - (halfHeight / 2)), halfWidth, halfHeight)));
+
+            ViewedPhoto.Source = _photo.Image;
+        }
+
+        private void BlackAndWhite(object sender, RoutedEventArgs e)
+        {
+            BitmapSource img = (BitmapSource)(_photo.Image);
+            _photo.Image = BitmapFrame.Create(new FormatConvertedBitmap(img, PixelFormats.Gray8, BitmapPalettes.Gray256, 1.0));
+
+            ViewedPhoto.Source = _photo.Image;
+        }
+
+        private void Border_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RotatePhoto();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState.Minimized==this.WindowState)
+            {
+                WindowState=WindowState.Normal;
+            }
+        }
+    }
+}

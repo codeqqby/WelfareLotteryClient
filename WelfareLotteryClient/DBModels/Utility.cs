eb3f120ce79d2d55103ab5445f071f0d7d3eb32e
test <test>
2015-06-18 11:54:55 +0800

    添加-->[两个相同的的属性进行属性值Copy] 和 扩展方法

--- WelfareLotteryClient/DBModels/Utility.cs
+++ WelfareLotteryClient/DBModels/Utility.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace WelfareLotteryClient.DBModels
@@ -10,16 +13,17 @@ namespace WelfareLotteryClient.DBModels
         /// <summary>
         /// 将图片数据转换为Base64字符串
         /// </summary>
-        //public string ToBase64(object img)
-        //{
-        //    //Image img = this.pictureBox.Image;
-        //    BinaryFormatter binFormatter = new BinaryFormatter();
-        //    MemoryStream memStream = new MemoryStream();
-        //    binFormatter.Serialize(memStream, img);
-        //    byte[] bytes = memStream.GetBuffer();
-        //    return Convert.ToBase64String(bytes);
-        //    //this.richTextBox.Text = base64;
-        //}
+        public byte[] ToBase64(object img)
+        {
+            //Image img = this.pictureBox.Image;
+            BinaryFormatter binFormatter = new BinaryFormatter();
+            MemoryStream memStream = new MemoryStream();
+            binFormatter.Serialize(memStream, img);
+            //byte[] bytes = 
+              return  memStream.GetBuffer();
+            //return Convert.ToBase64String(bytes);
+            //this.richTextBox.Text = base64;
+        }
 
         /// <summary>
         /// 将Base64字符串转换为图片
@@ -32,6 +36,9 @@ namespace WelfareLotteryClient.DBModels
         //    return (System.Drawing.Bitmap)binFormatter.Deserialize(memStream);
         //}
 
+        /// <summary>
+        /// 获取文件数组
+        /// </summary>
         public byte[] GetPictureData(string imagepath)
         {
             /**/
@@ -43,6 +50,11 @@ namespace WelfareLotteryClient.DBModels
             return byData;
         }
 
+        /// <summary>
+        /// 将字节数组转换为BitmapImage
+        /// </summary>
+        /// <param name="byteArray"></param>
+        /// <returns></returns>
         public BitmapImage ByteArrayToBitmapImage(byte[] byteArray)
         {
             BitmapImage bmp = null;
@@ -79,6 +91,48 @@ namespace WelfareLotteryClient.DBModels
                     }
                 }
             return byteArray;
+        }
+
+        /// <summary>
+        /// 两个相同的的属性进行属性值Copy
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public object CopyProperties(object origin, object target)
+        {
+            Type originType = origin.GetType();
+            Type targetType = target.GetType();
+            
+            foreach (PropertyInfo property in  originType.GetProperties())
+            {
+                if (!property.CanRead) continue;
+                var value = property.GetValue(origin);
+                var ta = targetType.GetProperty(property.Name);
+                if (ta!=null && property.PropertyType==ta.PropertyType && ta.CanWrite)
+                {
+                    ta.SetValue(target, value); 
+                }
+            }
+            return null;
+        }
+    }
+
+    public static class Tools
+    {
+        public static void MessageBoxDialog(this string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        public static string GetTextBoxText(this TextBox box)
+        {
+            return box.Text.Trim();
+        }
+
+        public static bool IsNullOrEmpty(this string msg)
+        {
+            return string.IsNullOrEmpty(msg);
         }
     }
 }

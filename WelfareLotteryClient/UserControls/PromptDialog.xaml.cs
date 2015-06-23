7b4e00c38cda8e7dd24206d8339a6e3c23af73a6
test <test>
2015-06-23 11:56:54 +0800

    PromptDialog

--- WelfareLotteryClient/UserControls/PromptDialog.xaml.cs
+++ WelfareLotteryClient/UserControls/PromptDialog.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WelfareLotteryClient.UserControls
 {
@@ -62,16 +50,7 @@ namespace WelfareLotteryClient.UserControls
             return null;
         }
 
-        public string ResponseText
-        {
-            get
-            {
-                if (_inputType == InputType.Password)
-                    return txtPasswordResponse.Password;
-                else
-                    return txtResponse.Text;
-            }
-        }
+        public string ResponseText => _inputType == InputType.Password ? txtPasswordResponse.Password : txtResponse.Text;
 
         private void btnOk_Click(object sender, RoutedEventArgs e)
         {

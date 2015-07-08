dc49e298ae9851ce3bb06718915843d196763394
test <test>
2015-07-08 15:28:21 +0800

    增加条件筛选和日志

--- WelfareLotteryClient/UserControls/LotteryStationInfo.xaml.cs
+++ WelfareLotteryClient/UserControls/LotteryStationInfo.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,7 +22,7 @@ namespace WelfareLotteryClient.UserControls
     public partial class LotteryStationInfo : UserControl
     {
         WelfareLotteryEntities entities;
-        private readonly List<LotteryStation> alLotteryStations;
+        private IEnumerable<LotteryStation> alLotteryStations;
 
         public LotteryStationInfo()
         {
@@ -28,9 +30,25 @@ namespace WelfareLotteryClient.UserControls
 
             entities = (WelfareLotteryEntities) Application.Current.Resources["WelfareLotteryEntities"];
 
-            alLotteryStations = entities.LotteryStations.ToList();
+            InitSearchQuery();
+
+            InitializeGrid();
+        }
+
+        private void InitSearchQuery()
+        {
+            cboXsStationCode.ItemsSource = entities.LotteryStations.Select(p => p.StationCode).ToList();
+            cboXsRegion.ItemsSource = entities.StationRegions.ToList();
+            cboXsGameType.ItemsSource = entities.WelfareLotteryGameTypes.ToList();
+            cboXsAdmin.ItemsSource = entities.Administrators.ToList();
+            cboXsManageTypeName.ItemsSource = entities.StationManageTypes.Select(p => p.TypeName).ToList();
+        }
+
+        private void InitializeGrid()
+        {
+            alLotteryStations =entities.LotteryStations.ToList();
             lvStationInfo.ItemsSource = alLotteryStations;
-            lvStationInfo.LoadingRow +=DataGridSoftware_LoadingRow;
+            lvStationInfo.LoadingRow += DataGridSoftware_LoadingRow;
         }
 
         private void DataGridSoftware_LoadingRow(object sender, DataGridRowEventArgs e)
@@ -45,9 +63,9 @@ namespace WelfareLotteryClient.UserControls
             LotteryStation station = alLotteryStations.FirstOrDefault(p => p.Id == Convert.ToInt32(btn.Tag));
             SalesclerkInfo info = new SalesclerkInfo(entities, station)
             {
-                Owner = Application.Current.MainWindow
+                Owner = Application.Current.MainWindow,
+                clerkListBox = {IsEnabled = Tools.LoginUserHasRights()}
             };
-            info.clerkListBox.IsEnabled = Tools.LoginUserHasRights();
             info.ShowDialog();
         }
 
@@ -80,6 +98,15 @@ namespace WelfareLotteryClient.UserControls
             if (MessageBox.Show("是否提交更改变？", "提交", MessageBoxButton.OKCancel) != MessageBoxResult.OK) return;
             var dicAllPhoto = viewer.Photos.ToDictionary(ph => ph.PhotoName, ph => ph.base64Value);
             station.StationPicListSerialized= JsonConvert.SerializeObject(dicAllPhoto);
+            LoginedUserInfo us = Tools.GetLoginedUserInfo();
+            entities.Logs.Add(new Log
+            {
+                UGuid = us.UGuid,
+                Username = us.UName,
+                Memo = $"更改编号为【{station.Id}】-网点编号为【{station.StationCode}】的网点照片",
+                OptType = (int)OptType.修改,
+                OptTime = DateTime.Now
+            });
             entities.SaveChanges();
         }
 
@@ -95,10 +122,6 @@ namespace WelfareLotteryClient.UserControls
                 adGameType = {IsEnabled = Tools.LoginUserHasRights()}
             };
             page.ShowDialog();
-            if (page.IsChanged)
-            {
-                entities.SaveChanges();
-            }
         }
 
         //网点变更信息
@@ -136,6 +159,15 @@ namespace WelfareLotteryClient.UserControls
             var result = type.GameTypeSelectedCollection;
             string serialize= JsonConvert.SerializeObject(result);
             s.WelfareGameTypeListSerialized = serialize;
+            LoginedUserInfo u = Tools.GetLoginedUserInfo();
+            entities.Logs.Add(new Log
+            {
+                UGuid = u.UGuid,
+                Username = u.UName,
+                Memo = $"编辑编号为【{s.Id}】-站点编号为【{s.StationCode}】的店面游戏类型",
+                OptType = (int)OptType.修改,
+                OptTime = DateTime.Now
+            });
             entities.SaveChanges();
             btn.Tag = serialize;
         }
@@ -169,6 +201,15 @@ namespace WelfareLotteryClient.UserControls
                 if (station.Violation == resultBox.Text) return;
                 if (MessageBoxResult.OK != MessageBox.Show("是否提交更改", "提示", MessageBoxButton.OKCancel)) return;
                 station.Violation = resultBox.Text;
+                LoginedUserInfo u = Tools.GetLoginedUserInfo();
+                entities.Logs.Add(new Log
+                {
+                    UGuid = u.UGuid,
+                    Username = u.UName,
+                    Memo = $"编辑编号为【{station.Id}】-站点编号为【{station.StationCode}】的违规信息",
+                    OptType = (int)OptType.修改,
+                    OptTime = DateTime.Now
+                });
                 entities.SaveChanges();
             };
             win.ShowDialog();
@@ -226,6 +267,15 @@ namespace WelfareLotteryClient.UserControls
                 if (viewer.IsChanged)
                     station.GuaranteeBase64IdentityPic =JsonConvert.SerializeObject(viewer.Photos.ToDictionary(key => key.PhotoName,value => value.base64Value));
                 btn.Content = station.GuaranteeName = inputBox.GetTextBoxText();
+                LoginedUserInfo u = Tools.GetLoginedUserInfo();
+                entities.Logs.Add(new Log
+                {
+                    UGuid = u.UGuid,
+                    Username = u.UName,
+                    Memo = $"编辑编号为【{station.Id}】-站点编号为【{station.StationCode}】的担保人信息",
+                    OptType = (int)OptType.修改,
+                    OptTime = DateTime.Now
+                });
                 entities.SaveChanges(); win.Close();
             };
 
@@ -245,7 +295,7 @@ namespace WelfareLotteryClient.UserControls
             HostInformation hostInfo = new HostInformation
             {
                 txtHostPhoneNum1 = {Text = hostPhoneNum[0]},
-                txtHostPhoneNum2 = {Text = hostPhoneNum[0]},
+                txtHostPhoneNum2 = {Text = hostPhoneNum[1]},
                 txtHostIdentityAddress = {Text = station.HostIdentityAddress},
                 txtHostName = {Text = station.HostName},
                 txtHostIdentityNo = {Text = station.HostIdentityNo},
@@ -268,15 +318,57 @@ namespace WelfareLotteryClient.UserControls
             };
             win.Closing += (a, b) =>
             {
+                bool changed = false;
+
                 string hostPhoneNum1 = hostInfo.txtHostPhoneNum1.GetTextBoxText();
                 string hostPhoneNum2 = hostInfo.txtHostPhoneNum2.GetTextBoxText();
 
-                station.HostName =hostInfo.txtHostName.GetTextBoxText();
-                station.HostPhoneNum = $"{hostPhoneNum1}⊙{hostPhoneNum2}";
-                station.HostIdentityNo = hostInfo.txtHostIdentityNo.GetTextBoxText();
-                station.HostIdentityAddress = hostInfo.txtHostIdentityAddress.GetTextBoxText();
-                if (null != hostInfo.HostPic) station.HostBase64Pic = hostInfo.HostPic;
-                if (hostInfo.hostIdentityPic.IsChanged) station.HostBase64IdentityPic = hostInfo.Identity;
+                string hostN = hostInfo.txtHostName.GetTextBoxText();
+                if (!hostN.IsNullOrEmpty())
+                {
+                    changed = station.HostName!=hostN;
+                    station.HostName = hostN;
+                    
+                }
+                if ((!hostPhoneNum1.IsNullOrEmpty() && !hostPhoneNum2.IsNullOrEmpty()))
+                {
+                    var t = $"{hostPhoneNum1}⊙{hostPhoneNum2}";
+                    changed = changed || station.HostPhoneNum!=t;
+                    station.HostPhoneNum = t;
+                }
+                if (!hostInfo.txtHostIdentityNo.GetTextBoxText().IsNullOrEmpty())
+                {
+                    changed = changed || station.HostIdentityNo != hostInfo.txtHostIdentityNo.GetTextBoxText();
+                    station.HostIdentityNo = hostInfo.txtHostIdentityNo.GetTextBoxText();
+                }
+                if (!hostInfo.txtHostIdentityAddress.GetTextBoxText().IsNullOrEmpty())
+                {
+                    changed = changed || station.HostIdentityAddress != hostInfo.txtHostIdentityAddress.GetTextBoxText();
+                    station.HostIdentityAddress = hostInfo.txtHostIdentityAddress.GetTextBoxText();
+                }
+                if (null != hostInfo.HostPic)
+                {
+                    station.HostBase64Pic = hostInfo.HostPic;
+                    changed = true;
+                }
+                if (hostInfo.hostIdentityPic.IsChanged)
+                {
+                    station.HostBase64IdentityPic = hostInfo.Identity;
+                    changed = true;
+                }
+
+                if (changed)
+                {
+                    LoginedUserInfo us = Tools.GetLoginedUserInfo();
+                    entities.Logs.Add(new Log
+                    {
+                        UGuid = us.UGuid,
+                        Username = us.UName,
+                        Memo = $"编辑编号为【{station.Id}】-站点编号为【{station.StationCode}】的机主信息",
+                        OptType = (int) OptType.修改,
+                        OptTime = DateTime.Now
+                    });
+                }
 
                 entities.SaveChanges();
                 btn.Content = station.HostName;
@@ -295,6 +387,68 @@ namespace WelfareLotteryClient.UserControls
             details.ShowDialog();
             lvStationInfo.SelectedItem = null; //防止点击过一次后点击空白处还会自发 只有再次重新选择后才可以
             e.Handled = true;
+            details.Close();
+        }
+
+        //刷新
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            InitializeGrid();
+        }
+
+        private IEnumerable<LotteryStation> gridTempData;
+        //查询
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            if (!cboXsStationCode.Text.IsNullOrEmpty())
+            {
+                gridTempData = alLotteryStations.Where(p => p.StationCode == cboXsStationCode.Text);
+            }
+
+            var region = cboXsRegion.SelectedItem as StationRegion;
+            if (null!=region)
+            {
+                gridTempData = (gridTempData??alLotteryStations).Where(p => p.StationRegion == region);
+            }
+
+            var gameType = cboXsGameType.SelectedItem as WelfareLotteryGameType;
+            if (null!=gameType)
+            {
+                gridTempData = (gridTempData ?? alLotteryStations).Where(p => p.WelfareGameTypeListSerialized.Contains(Convert.ToString(gameType.Id)));
+            }
+
+            var admin = cboXsAdmin.SelectedItem as Administrator;
+            if (null!=admin)
+            {
+                gridTempData= (gridTempData ?? alLotteryStations).Where(p => p.Administrator.Id==admin.Id);
+            }
+
+            var rights = cboXsRights.Text;
+            if (!rights.IsNullOrEmpty())
+            {
+                gridTempData = (gridTempData ?? alLotteryStations).Where(p => p.PropertyRight==Convert.ToBoolean(cboXsRights.SelectedIndex));
+            }
+
+            var machine = cboXsMachineType.Text;
+            if (!machine.IsNullOrEmpty())
+            {
+                gridTempData = (gridTempData ?? alLotteryStations).Where(p => p.MachineType == Convert.ToBoolean(cboXsMachineType.SelectedIndex));
+            }
+
+            var communication = cboXsCommunicationType.Text;
+            if (!communication.IsNullOrEmpty())
+            {
+                gridTempData = (gridTempData ?? alLotteryStations).Where(p => p.CommunicationType == Convert.ToBoolean(cboXsCommunicationType.SelectedIndex));
+            }
+
+            if (!cboXsManageTypeName.Text.IsNullOrEmpty())
+            {
+                gridTempData = (gridTempData ?? alLotteryStations).Where(p => p.ManageTypeName == cboXsManageTypeName.Text);
+            }
+            if (null == gridTempData) return;
+            lvStationInfo.ItemsSource = gridTempData;
+            lvStationInfo.LoadingRow += DataGridSoftware_LoadingRow;
+            gridTempData = null;
         }
     }
 
